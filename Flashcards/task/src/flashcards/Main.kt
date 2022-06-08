e227e9fbfb776c2a050575664982223ae63245b0
ahmedhosnypro <ahmedhosnypro@gmail.com>
2022-06-08 20:42:42 +0200

    Make it your own

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,10 +1,26 @@
 package flashcards
 
 fun main() {
-    val term = readln()
-    if (readln() == readln()) {
-        println("right")
-    } else {
-        println("wrong")
+    println("Input the number of cards:")
+    val cards = readln().toInt()
+
+    val terms = mutableListOf<String>()
+    val definitions = mutableListOf<String>()
+
+    for (i in 1..cards) {
+        println("Card #$i:")
+        terms.add(readln())
+        println("The definition for card #$i:")
+        definitions.add(readln())
+    }
+
+    for (i in 0 until cards) {
+        println("Print the definition of \"${terms[i]}\"")
+        val userInput = readln()
+        if (userInput == definitions[i]) {
+            println("Correct!")
+        } else {
+            println("Wrong. The right answer is \"${definitions[i]}\".")
+        }
     }
 }
